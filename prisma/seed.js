3ea62530672f56af1865d56f1be87804f4e1bcb4
rahad-dev24 <rahad.dev24@gmail.com>
2024-06-29 15:08:47 +0600

    added session auth code with redis store in server.js file

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,55 +1,54 @@
 import { PrismaClient } from "@prisma/client";
-import { create } from "domain";
 const prisma = new PrismaClient();
 async function main() {
-  const product = await prisma.product.create({
-    data: {
-      product_name: "product12wwww",
-      price: 1000,
-      rent_price: 100,
-      description: "descriptionwwwwwwwww",
-      user_id: "clxys8wh40000147i9kxa5aj2",
-      category: {
-        create: [
-          {
-            category_id: "clxybn62d000059yp8ysskgiy",
-          },
-          {
-            category_id: "clxybn62d000159ypqqzkz8oq",
-          },
-        ],
+  await prisma.category.createMany({
+    data: [
+      {
+        category_name: "ELECTRONICS",
       },
-    },
-  });
-}
-//  await prisma.category.createMany({
-//    data: [
-//      {
-//        category_name: "ELECTRONICS",
-//      },
-//
-//      {
-//        category_name: "FURNITURE",
-//      },
-//
-//      {
-//        category_name: "HOME APPLIANCES",
-//      },
-//
-//      {
-//        category_name: "SPORTING GOOD",
-//      },
-//
-//      {
-//        category_name: "OUTDOOR",
-//      },
-//
-//      {
-//        category_name: "TOYS",
-//      },
-//    ],
-//  });
 
+      {
+        category_name: "FURNITURE",
+      },
+
+      {
+        category_name: "HOME APPLIANCES",
+      },
+
+      {
+        category_name: "SPORTING GOOD",
+      },
+
+      {
+        category_name: "OUTDOOR",
+      },
+
+      {
+        category_name: "TOYS",
+      },
+    ],
+  });
+
+  //  const product = await prisma.product.create({
+  //    data: {
+  //      product_name: "product12wwww",
+  //      price: 1000,
+  //      rent_price: 100,
+  //      description: "descriptionwwwwwwwww",
+  //      user_id: "clxys8wh40000147i9kxa5aj2",
+  //      category: {
+  //        create: [
+  //          {
+  //            category_id: "clxybn62d000059yp8ysskgiy",
+  //          },
+  //          {
+  //            category_id: "clxybn62d000159ypqqzkz8oq",
+  //          },
+  //        ],
+  //      },
+  //    },
+  //  });
+}
 main()
   .then(async () => {
     await prisma.$disconnect();
